d0f58fa4221971e439ccebb998c78f1a13f22301
erich <erich@88.com>
2024-09-03 15:31:17 +0800

    支持groq

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -6,7 +6,7 @@ export const config = {
 
 export default async function handler(req: NextRequest) {
   const url = new URL(req.url);
-  url.host = "api.openai.com";
+  url.host = "api.groq.com/openai";
   url.protocol = "https:";
   url.port = "";
 
